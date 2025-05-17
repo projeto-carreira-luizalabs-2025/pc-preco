c8c0169a3b8d494f527f338b254a203610178099
eduardorib <eduardoribeirogon@gmail.com>
2025-05-17 16:27:24 -0300

    refactor: cria memória já como lista de dicionários

--- app/container.py
+++ app/container.py
@@ -6,11 +6,9 @@ from app.services import HealthCheckService, PriceService
 from app.settings import AppSettings
 
 memory_prices = [
-    Price(seller_id="1", sku="A", preco_de=100, preco_por=90),
-    Price(seller_id="2", sku="B", preco_de=200, preco_por=180),
+    {"seller_id": "1", "sku": "A", "preco_de": 100, "preco_por": 90},
+    {"seller_id": "2", "sku": "B", "preco_de": 200, "preco_por": 180},
 ]
-
-price_dicts = [p.model_dump() for p in memory_prices]
 
 
 class Container(containers.DeclarativeContainer):
@@ -19,7 +17,7 @@ class Container(containers.DeclarativeContainer):
     settings = providers.Singleton(AppSettings)
 
     # Repositórios
-    price_repository = providers.Singleton(PriceRepository, memory=price_dicts)
+    price_repository = providers.Singleton(PriceRepository, memory=memory_prices)
 
     # Serviços
     health_check_service = providers.Singleton(
