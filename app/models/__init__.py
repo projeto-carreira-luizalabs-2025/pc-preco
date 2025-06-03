5c8c590358131f8c513cf7bd87c034af19fd47f8
eduardorib <eduardoribeirogon@gmail.com>
2025-06-03 20:38:06 -0300

    feat: adiciona e adapta models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,26 @@
-from .base import AuditModel, PersistableEntity, UuidModel, UuidType
+from .base import (
+    IntModel,
+    PersistableEntity,
+    SelllerSkuIntPersistableEntity,
+    SelllerSkuUuidPersistableEntity,
+    UuidModel,
+    UuidPersistableEntity,
+    AuditModel,
+    UuidType,
+)
+
+from .query import QueryModel
 from .price_model import Price
 
-__all__ = ["AuditModel", "PersistableEntity", "UuidModel", "UuidType", "Price"]
+__all__ = [
+    "AuditModel",
+    "PersistableEntity",
+    "UuidModel",
+    "UuidType",
+    "Price",
+    "QueryModel",
+    "IntModel",
+    "UuidPersistableEntity",
+    "SelllerSkuUuidPersistableEntity",
+    "SelllerSkuIntPersistableEntity",
+]
--- app/models/base.py
+++ app/models/base.py
@@ -7,6 +7,14 @@ from uuid_extensions import uuid7
 from app.common.datetime import utcnow
 
 
+class IdModel(BaseModel):
+    id: UuidType | int | None = Field(None, description="Chave")
+
+
+class IntModel(IdModel):
+    id: int | None = Field(None, description="Identificador")
+
+
 class UuidModel(BaseModel):
     id: UuidType = Field(default_factory=uuid7, alias="_id")
 
@@ -21,9 +29,41 @@ class AuditModel(BaseModel):
     audit_updated_at: datetime | None = Field(None, description="Data e hora da efetiva atualização do registro")
 
 
-class PersistableEntity(UuidModel, AuditModel):
+class PersistableEntity(IdModel, AuditModel):
     model_config = ConfigDict(populate_by_name=True, from_attributes=True)
 
     @classmethod
     def from_json(cls, json_data: str):
         return TypeAdapter(cls).validate_json(json_data)
+
+
+class UuidPersistableEntity(PersistableEntity, UuidModel):
+    """
+    Entidade cuja chave é um uuid
+    """
+
+
+class IntPersistableEntity(PersistableEntity, IntModel):
+    """
+    Entidiade cuja chave é um inteiro.
+    """
+
+
+class SellerSkuEntity(BaseModel):
+    seller_id: str = Field(..., description="ID do seller")
+    sku: str = Field(..., description="ID do produto")
+
+    def get_sellerid_sku(self) -> dict:
+        return {"seller_id": self.seller_id, "sku": self.sku}
+
+
+class SelllerSkuUuidPersistableEntity(SellerSkuEntity, UuidPersistableEntity):
+    """
+    Entidade com seller_id e sku e chave uuid.
+    """
+
+
+class SelllerSkuIntPersistableEntity(SellerSkuEntity, IntPersistableEntity):
+    """
+    Entidade com seller_id e sku e chave int.
+    """
