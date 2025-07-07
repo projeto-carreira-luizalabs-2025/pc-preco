84a1a07646cd46f0adcab0c0d68ead51fc2b5136
eduardorib <eduardoribeirogon@gmail.com>
2025-07-07 17:09:14 -0300

    feat: adiciona função para obtenção dos últimos n preços do histórico

--- app/services/price_history_service.py
+++ app/services/price_history_service.py
@@ -10,8 +10,9 @@ LoggingBuilder.init(log_level="DEBUG")
 
 logger = LoggingBuilder.get_logger(__name__)
 
+
 class PriceHistoryService(CrudService[PriceHistory]):
-    
+
     def __init__(self, repository: PriceHistoryRepository):
         super().__init__(repository)
 
@@ -25,7 +26,7 @@ class PriceHistoryService(CrudService[PriceHistory]):
         :return: Lista de instâncias de PriceHistory.
         :raises PriceNotFoundException: Se não houver histórico.
         """
-        
+
         logger.info(f"Buscando histórico de preços para seller_id: {seller_id}, sku: {sku}")
 
         filters = PriceFilter(seller_id=seller_id, sku=sku)
@@ -36,6 +37,24 @@ class PriceHistoryService(CrudService[PriceHistory]):
             raise PriceNotFoundException(seller_id=seller_id, sku=sku)
 
         logger.debug(f"Encontrados {len(results)} registros de histórico")
-        
+
         return results
-    
+
+    async def get_last_n_prices(self, seller_id: str, sku: str, n: int = 5) -> list[PriceHistory]:
+        """
+        Recupera os últimos n preços de um produto por seller_id e sku.
+        """
+
+        logger.info(f"Recuperando os últimos {n} preços para seller_id: {seller_id}, sku: {sku}")
+
+        filters = PriceFilter(seller_id=seller_id, sku=sku)
+        # Cria um paginator manualmente, ordenando por registered_at desc
+        paginator = Paginator(request_path="/", limit=n, offset=0, sort="registered_at:desc")
+        results = await self.find(filters=filters, paginator=paginator)
+
+        if not results:
+            logger.warning(f"Nenhum histórico encontrado para seller_id: {seller_id}, sku: {sku}")
+            raise PriceNotFoundException(seller_id=seller_id, sku=sku)
+
+        logger.debug(f"Encontrados {len(results)} registros de histórico")
+        return results
