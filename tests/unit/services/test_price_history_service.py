443b4e83f6a49c3807cdc9d27f1141c55330d73f
eduardorib <eduardoribeirogon@gmail.com>
2025-07-08 20:29:36 -0300

    feat: adiciona testes para price_history_service

---
+++ tests/unit/services/test_price_history_service.py
@@ -0,0 +1,48 @@
+import pytest
+from unittest.mock import AsyncMock, MagicMock
+from app.services.price_history_service import PriceHistoryService
+from app.common.exceptions.price_exceptions import PriceNotFoundException
+
+
+@pytest.fixture
+def repository():
+    return AsyncMock()
+
+
+@pytest.fixture
+def service(repository):
+    return PriceHistoryService(repository)
+
+
+@pytest.mark.asyncio
+async def test_get_by_seller_id_and_sku_success(service, repository):
+    paginator = MagicMock()
+    fake_history = [MagicMock(), MagicMock()]
+    repository.find.return_value = fake_history
+    result = await service.get_by_seller_id_and_sku("1", "A", paginator)
+    repository.find.assert_awaited()
+    assert result == fake_history
+
+
+@pytest.mark.asyncio
+async def test_get_by_seller_id_and_sku_not_found(service, repository):
+    paginator = MagicMock()
+    repository.find.return_value = []
+    with pytest.raises(PriceNotFoundException):
+        await service.get_by_seller_id_and_sku("1", "A", paginator)
+
+
+@pytest.mark.asyncio
+async def test_get_last_n_prices_success(service, repository):
+    fake_history = [MagicMock(), MagicMock()]
+    repository.find.return_value = fake_history
+    result = await service.get_last_n_prices("1", "A", n=2)
+    repository.find.assert_awaited()
+    assert result == fake_history
+
+
+@pytest.mark.asyncio
+async def test_get_last_n_prices_not_found(service, repository):
+    repository.find.return_value = []
+    with pytest.raises(PriceNotFoundException):
+        await service.get_last_n_prices("1", "A", n=2)
